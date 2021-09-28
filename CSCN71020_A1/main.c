2d43ce5e57320c8be93aca99771b3ff9e1bd12d7
Muhammad Rao <mtrao2003@gmail.com>
2021-09-27 20:03:55 -0400

    changes the hard coded numbers for subtractions into user input

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,7 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
-
+void subtract();
 
 void main() {
 
@@ -14,29 +14,34 @@ void main() {
 	int inputNum;
 
 	printf("Enter operation number: ");
-	scanf_s("%1o", &inputNum);
+	scanf_s("%d", &inputNum);
+
 
 	switch (inputNum)
 	{
 	case 1:
 		add();
-	}
-
+		break;
+	case 2:
+		subtract();
+		break;
+	default:;
+	};
 }
 
-void printWelcomeMenu() {
+	void printWelcomeMenu() {
 	printf(" **********************\n");
 	printf("**   Welcome to the   **\n");
 	printf("**   BCS Calculator   **\n");
 	printf(" **********************\n");
 }
 
-void printOptions() {
+	void printOptions() {
 	printf("1. Add\n");
 	printf("2. Subtract\n");
 }
 
-void add() {
+	void add() {
 	double num1, num2, result;
 	printf("Enter the first value:");
 	scanf_s("%lf", &num1);
@@ -44,4 +49,15 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+	void subtract() {
+	double num1, num2, result;
+
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
